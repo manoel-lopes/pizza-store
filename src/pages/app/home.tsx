fe83411c584d713f0114ed21b01497f2633a16bb
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-01 14:39:20 -0300

    feat(pages): add titles to home and sign-in pages
    Adds the Helmet component to the home and sign-in pages to set their respective document titles.

--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -1,3 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <Helmet title='home' />
+      <h1>Home</h1>
+    </>
+  )
 }
