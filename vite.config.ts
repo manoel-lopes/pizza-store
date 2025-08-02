28dcf45cb1b270114b460ed904072c3d373d5a27
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-02 13:37:55 -0300

    style(vite): remove empty line

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { defineConfig } from 'vitest/config'
-
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
