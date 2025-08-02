55778d74eaae3652369675e67e095e27fcc5edf3
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-02 12:54:53 -0300

    feat(auth): enhance sign-in page
    Replaces the basic sign-in page with a structured layout, including a clear call-to-action and the new sign-in form.

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,22 @@
 import { Helmet } from 'react-helmet-async'
 
+import { SignInForm } from './sign-in-form'
+
 export function SignIn() {
   return (
     <>
       <Helmet title='signin' />
-      <h1>SignIn</h1>
+      <div className='p-8'>
+        <div className='w-[350px] flex flex-col justify-center gap-6'>
+          <div className='flex flex-col gap-2 text-center'>
+            <h1 className='text-2xl font-semibold tracking-tight'>Sign in to your account</h1>
+            <p className='text-sm text-muted-foreground'>
+              Enter your email below to sign in to your account
+            </p>
+          </div>
+          <SignInForm />
+        </div>
+      </div>
     </>
   )
 }
