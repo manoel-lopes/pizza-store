a6494009a6b49fafdc21787d0197b11d93fcd52c
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-04 16:25:57 -0300

    feat(router): add orders page route

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { AppLayout } from '@/pages/_layouts/app'
-import { AuthLayout } from '@/pages/_layouts/auth'
-import { Home } from '@/pages/app/home'
-import { SignIn } from '@/pages/auth/sign-in'
-import { SignUp } from '@/pages/auth/sign-up'
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
+import { Home } from './pages/app/home'
+import { Orders } from './pages/auth/orders/orders'
+import { SignIn } from './pages/auth/sign-in/sign-in'
+import { SignUp } from './pages/auth/sign-up/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ export const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: '/', element: <Home /> },
+      { path: '/orders', element: <Orders /> },
     ],
   },
   {
