8cc1a0f98aa04f48f01c4fafade5f72d3cda91fa
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-05 13:57:18 -0300

    feat(order): add order details dialog

---
+++ src/pages/auth/orders/order-details.tsx
@@ -0,0 +1,40 @@
+import type { Order } from '@/core/domain/oder'
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+} from '@/components/ui/table'
+import { OrderStatusBadge } from './order-status-badge'
+
+type OrderDetailsDialogProps = {
+  order: Order
+}
+
+export function OrderDetailsDialog({ order }: OrderDetailsDialogProps) {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Order {order.id}</DialogTitle>
+        <DialogDescription>Order details</DialogDescription>
+      </DialogHeader>
+      <div className='space-y-6'>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <OrderStatusBadge status={order.status} />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </DialogContent>
+  )
+}
