7bc1a976d11a41a1fd50d0998eb5dd5fd417ea40
manoel-lopes <manoel.lopes.full@gmail.com>
2025-08-05 15:03:30 -0300

    feat(order): update order client type to User

--- src/core/domain/order.ts
+++ src/core/domain/order.ts
@@ -1,9 +1,11 @@
+import type { User } from './user'
+
 export type OrderStatus = 'pending' | 'processing' | 'delivering' | 'delivered' | 'cancelled'
 
 export type Order = {
   id: number
   createdAt: string
   status: OrderStatus
-  client: string
+  client: User
   total: number
 }
